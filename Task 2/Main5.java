304722cd06200748412a531ce02a73bd08e72ede
syedtamzidakib <tamzidakib77@gmail.com>
2026-06-18 16:43:08 +0600

    Update Main5.java

--- Task 2/Main5.java
+++ Task 2/Main5.java
@@ -1,1 +1,15 @@
+class Printer{
+    void printData(String text){
+        System.out.println(text);}
+    void printData(int number){
+        System.out.println(number);
+    }
+}
+public class Main5 {
+    public static void main(String[] args) {
+        Printer p = new Printer();
+        p.printData("Hello printer");
+        p.printData(123);
+    }
+}
 
