74f2e8b2c7393b09bd24ec8fe944dab807d4502e
syedtamzidakib <tamzidakib77@gmail.com>
2026-06-18 16:07:36 +0600

    Update Main1.java

--- Task 2/Main1.java
+++ Task 2/Main1.java
@@ -1,1 +1,12 @@
-
+class ContestStats{
+int[] scores= {85,90,95};
+void printFirstScore(){
+    System.out.println(scores[0]);
+}
+}
+public class Main1{
+    public static void main(String[] args) {
+        ContestStats stats = new ContestStats();
+        stats.printFirstScore();    
+    }
+}
