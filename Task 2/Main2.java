b8c96b6dc8d1f0dbd77700c11132252e0e1e053b
syedtamzidakib <tamzidakib77@gmail.com>
2026-06-18 16:21:31 +0600

    Update Main2.java

--- Task 2/Main2.java
+++ Task 2/Main2.java
@@ -1,1 +1,16 @@
+class NumberChecker{
+    void check (int num){
+        boolean result = (num % 2 == 0) && (num >10);
+        System.out.println(result);
+    
+    }
+
+}
+public class Main2 {
+    public static void main(String[] args) {
+        NumberChecker obj = new NumberChecker();
+        obj.check(14);
+    }
+    
+}
 
