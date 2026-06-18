1ac5d21d6a76c0412f1e832df06dd31f50b29cc4
syedtamzidakib <tamzidakib77@gmail.com>
2026-06-18 16:49:14 +0600

    Update Main6.java

--- Task 2/Main6.java
+++ Task 2/Main6.java
@@ -1,1 +1,20 @@
+class Applet{
+void launch(){
+    System.out.println("Applet launched");
+}}
+class Gameapplet extends Applet{
+    
+    void play(){
+        System.out.println("Playing game");}
+}
+public class Main6 {
+     public static void main(String[] args) {
+
+        Gameapplet game = new Gameapplet();
+
+        game.launch();  
+        game.play();    
+    }
+    
+}
 
