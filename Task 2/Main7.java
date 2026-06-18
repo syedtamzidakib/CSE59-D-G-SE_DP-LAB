9b419e672ad99ed1571977d32dafe502415bf608
syedtamzidakib <tamzidakib77@gmail.com>
2026-06-18 16:53:44 +0600

    Update Main7.java

--- Task 2/Main7.java
+++ Task 2/Main7.java
@@ -1,1 +1,17 @@
+interface Clickable{
+    void onClick();
+}
+class Button implements Clickable{
+    @Override
+    public void onClick() {
+        System.out.println("Button was clicked!");
+    }
+}
+public class Main7 {
+    public static void main(String[] args) {
+        Button b = new Button();
+        b.onClick();
+    }
+    
+}
 
