a3f065dd08b8067a31cb779b2b015f3c0750b744
syedtamzidakib <tamzidakib77@gmail.com>
2026-07-23 15:48:54 +0600

    Update Main1.java

--- Task 8 (Lab exam 1 practice)/Main1.java
+++ Task 8 (Lab exam 1 practice)/Main1.java
@@ -1,1 +1,36 @@
+import java.util.Scanner;
 
+class Course{
+    String highestName ="";
+    double highestMarks = 0;
+    int topCount = 0;
+
+    void addStudent(String name, double marks){
+        if(marks> highestMarks){
+            highestMarks = marks;
+            highestName = name;
+        }
+        if(marks >=80){
+            topCount++;
+        }
+    }
+    String getHighestScorer(){
+        return highestName + "(" +(int)highestMarks+")";
+    }
+    int countTopGrades(double  threshhold){
+        return topCount;
+    }
+}
+public class Main1{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        input.next();
+        Course course = new Course();
+        course.addStudent(input.next(), input.nextDouble());
+        course.addStudent(input.next(), input.nextDouble());
+        course.addStudent(input.next(), input.nextDouble());
+
+        System.out.println("Highest Scorer: " + course.getHighestScorer());
+        System.out.println("Number of A+ (80+): " + course.countTopGrades(80));
+    }
+}
